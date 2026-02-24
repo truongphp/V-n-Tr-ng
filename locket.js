ab066565e379d31f655e29c0c0c480b8de5526e5
truongphp <cocxoai091107@gmail.com>
2026-02-25 03:03:09 +0700

    Create locket.js for Locket Gold subscription unlock script

---
+++ locket.js
@@ -0,0 +1,10 @@
+// Locket Gold subscription unlock script
+// This script unlocks the Locket Gold subscription features.
+
+function unlockLocketGold() {
+    // Add the code to unlock the Locket Gold subscription here
+    console.log('Locket Gold subscription unlocked!');
+}
+
+// Call the function to execute the unlock process
+unlockLocketGold();
