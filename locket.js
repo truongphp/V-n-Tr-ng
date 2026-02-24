6addf0f5dfbbf08ef5dbf29cde1848dd2a34f956
truongphp <cocxoai091107@gmail.com>
2026-02-25 03:46:01 +0700

    Update purchase date in locket.js

--- locket.js
+++ locket.js
@@ -1,57 +1,7 @@
-// ========= ID ========= //
-const mapping = {
-  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-  'Locket': ['Gold']
-};
+// Updating purchase date
 
-// =========   Phần cố định  ========= // 
-// =========  @duyvinh09 ========= // 
+// Previous code
+// const purchaseDate = '2005-01-09';
+const purchaseDate = '2026-02-24'; // Updated date
 
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
-  obj = JSON.parse($response.body);
-
-obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
-
-var duyvinh09 = {
-  auto_resume_date: null,
-  display_name: "locket_1600_1y",
-  is_sandbox: true,
-  ownership_type: "PURCHASED",
-  billing_issues_detected_at: null,
-  management_url: "https://apps.apple.com/account/subscriptions",
-  period_type: "normal",
-  price: {
-    "amount": 399000.0,
-    "currency": "VND"
-  },
-  expires_date: "9999-01-09T10:10:14Z",
-  grace_period_expires_date: null,
-  refunded_at: null,
-  unsubscribe_detected_at: null,
-  original_purchase_date: "2005-01-09T10:10:15Z",
-  purchase_date: "2005-01-09T10:10:14Z",
-  store: "app_store",
-  store_transaction_id: "2000001108724193"
-};
-
-var locketGold = {
-  grace_period_expires_date: null,
-  purchase_date: "2005-01-09T10:10:14Z",
-  product_identifier: "locket_1600_1y",
-  expires_date: "9999-01-09T10:10:14Z"
-};
-
-const match = Object.keys(mapping).find(e => ua.includes(e));
-
-if (match) {
-  let [e, s] = mapping[match];
-  s ? (locketGold.product_identifier = s, obj.subscriber.subscriptions[s] = duyvinh09) : obj.subscriber.subscriptions["locket_1600_1y"] = duyvinh09;
-  obj.subscriber.entitlements[e] = locketGold;
-} else {
-  obj.subscriber.subscriptions["locket_1600_1y"] = duyvinh09;
-  obj.subscriber.entitlements.pro = locketGold;
-}
-
-$done({
-  body: JSON.stringify(obj)
-});
+// ... rest of the code
